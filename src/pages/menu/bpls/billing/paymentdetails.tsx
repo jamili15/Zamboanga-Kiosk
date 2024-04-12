37c4abbcc3eb86fa072cd69c2ff074b1e6c68a9a
James Kevin Jamili <jameskevin.ramesessystems@gmail.com>
2024-04-12 14:30:26 +0800

    first commit

---
+++ src/pages/menu/bpls/billing/paymentdetails.tsx
@@ -0,0 +1,15 @@
+import Keyboard from "@/components/keyboard/Keyboard";
+import PaymentInformation from "@/components/transactions/bpls/payment/Info";
+
+import Layout from "./layout";
+
+const PaymentPage = () => {
+  return (
+    <Layout>
+      <PaymentInformation />
+      <Keyboard />
+    </Layout>
+  );
+};
+
+export default PaymentPage;
---
+++ src/stores/lgu-info.js
@@ -0,0 +1,40 @@
+export const landingInfo = [
+  {
+    logo: {
+      src: "/_custom/lgu-logo.jpg",
+      width: 120,
+      height: 120,
+    },
+    header: {
+      title: "city of puerto princesa",
+    },
+    subheader: {
+      title: "self service kiosk",
+    },
+  },
+];
+
+export const ticketInfo = [
+  {
+    logo: {
+      src: "/_custom/lgu-logo.jpg",
+      width: 80,
+    },
+    header: {
+      title: "Republic of the Philippines",
+    },
+    subheader: {
+      title: "city of puerto princesa",
+    },
+  },
+];
+
+export const landingBgLogo = [
+  {
+    logo: {
+      src: "/_custom/lp-bg-image.png",
+      width: 1000,
+      height: 1000,
+    },
+  },
+];
